bbf190e71072bf4fbcdbe5edcb8b3afb490c3d27
Ayub-123 <ayubursifar@gmail.com>
2022-06-18 12:10:22 +0600

    first commit

---
+++ home.php
@@ -0,0 +1,84 @@
+    <?php
+
+        // $a = 5;
+
+        // $b = 7;
+
+        // $c = 7;
+
+        // if(isset($a) && isset($b) && isset($c)){
+        //     echo 10;
+        //
+
+
+       
+      
+
+
+
+   ?>
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Document</title>
+   </head>
+   <body>
+
+       <h1 style="text-align:center;">Shahzadpur Vision</h1>
+      
+        <div style="text-align:center;">
+
+             <tr style="text-align:center">
+                <th class="border">
+                    <a href="">News Details</a>
+                </th> 
+
+                <th>
+                    <a href="">About Us</a>
+                </th>
+
+                <th>
+                    <a href="">Contact Us</a>
+                </th>
+
+                <!-- <th>
+                    <a href="">বাংলাদেশ</a>
+                </th>
+
+                <th>
+                    <a href="">বিশ্ব</a>
+                </th>
+
+                <th>
+                    <a href="">বাণিজ্য</a>
+                </th>
+
+                <th></th>
+                    <a href="">মতামত</a>
+                </th>
+
+                <th>
+                    <a href="">চাকরি</a>
+                </th> -->
+            </tr>
+        </div> <br>
+
+   
+        <div class="dropdown">
+              <button class="dropbtn">Account</button>
+                <div class="dropdown-content">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </div>
+
+        
+
+        </div>
+
+     
+       
+   </body>
+   </html>
---
+++ members.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <table border="1px solid black">
+        <tr>
+            <th>serial</th>
+            <th>name</th>
+            <th>phone</th>
+            <th>email</th>
+            <th>country</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Rabiul Islam</td>
+            <td>01700000000</td>
+            <td>rabiul@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Shafiul Alam</td>
+            <td>01700000000</td>
+            <td>shafiul@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Saiful Islam</td>
+            <td>01700000000</td>
+            <td>saiful@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Rashed Rifat</td>
+            <td>01700000000</td>
+            <td>rashed@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Saif Uddin</td>
+            <td>01700000000</td>
+            <td>saif@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Abid Rahman</td>
+            <td>01700000000</td>
+            <td>abid@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Shahnewaz Ibrahim</td>
+            <td>01700000000</td>
+            <td>himu@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Rashed</td>
+            <td>01700000000</td>
+            <td>rased@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>Sahin</td>
+            <td>01700000000</td>
+            <td>sahin@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>Ayubur Rahman</td>
+            <td>01700000000</td>
+            <td>ayubur@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        <tr>
+            <td>11</td>
+            <td>Mahdi</td>
+            <td>01700000000</td>
+            <td>mahdi@gmail.com</td>
+            <td>bangladesh</td>
+        </tr>
+        
+
+    </table>
+
+</body>
+</html>
+
